8d1feec3c2522ca86e28211156ceacafb384e42e
inshad <minshad430@gmail.com>
2025-06-02 04:16:59 +0530

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express')
 const mongoose=require('mongoose')
 const cors=require('cors')
 require('dotenv').config()
-const carRoutes=require('../backend/routes/carRoutes')
+const carRoutes=require('../../backend/routes/carRoutes')
 
 const app=express();
 
